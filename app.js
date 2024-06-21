3c2a7b7aecf25b1d4458a1e8ffe5e95bde292a02
yuyadaruma24 <166777024+yuyadaruma24@users.noreply.github.com>
2024-06-21 10:37:49 +0900

    expresshenkou

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/hello", (req, res) => res.type('html').send(html));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
@@ -54,7 +54,7 @@ const html = `
   </head>
   <body>
     <section>
-      Hello from Render!
+      Hello from Render! ネトプロ
     </section>
   </body>
 </html>
